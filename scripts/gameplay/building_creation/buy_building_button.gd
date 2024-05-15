7feadb9e5338c9c8bd5c01c4be4baf287e0e4459
Bocdagla <tomeu1m1a1@gmail.com>
2024-05-15 17:46:36 +0200

    Add database

--- scripts/gameplay/building_creation/buy_building_button.gd
+++ scripts/gameplay/building_creation/buy_building_button.gd
@@ -3,8 +3,7 @@ extends Button;
 
 @onready var _resource_manager: ResourceManager = %ResourceManager;
 
-@export var gold_cost: int = 100;
-@export var defense_cost: int = 0;
+@export var buildin_record: BuildingRecord;
 
 @export_category("UI")
 @onready var _gold_label: Label = $MarginContainer/VBoxContainer/Label/GoldLabel;
@@ -16,25 +15,27 @@ var _pressed_callables: Array[Callable];
 
 func _ready() -> void:
 	assert(_resource_manager);
+	assert(buildin_record);
 	var pressed_connections = pressed.get_connections();
 	for con in pressed_connections:
 		_pressed_callables.append(con.callable);
 		pressed.disconnect(con.callable);
 
 	_resource_manager.value_changed.connect(_set_available);
-	_gold_label.text = str(gold_cost);
-	_defense_label.text = str(defense_cost);
-	if defense_cost == 0:
+	text = buildin_record.name;
+	_gold_label.text = str(buildin_record.gold_cost);
+	_defense_label.text = str(buildin_record.defense_cost);
+	if buildin_record.defense_cost == 0:
 		_defense_label.visible = false;
 		_defense_icon.visible = false;
 
 
 func _pressed() -> void:
-	if _resource_manager.is_affordable(gold_cost, defense_cost):
-		_resource_manager.purchase(gold_cost);
+	if _resource_manager.is_affordable(buildin_record.gold_cost, buildin_record.defense_cost):
+		_resource_manager.purchase(buildin_record.gold_cost);
 		for callable in _pressed_callables:
 			if callable != null:
 				callable.call();
 
 func _set_available(_gold: int, _defense: int) -> void:
-	disabled = !_resource_manager.is_affordable(gold_cost, defense_cost);
+	disabled = !_resource_manager.is_affordable(buildin_record.gold_cost, buildin_record.defense_cost);
