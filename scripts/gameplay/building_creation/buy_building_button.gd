ded867e447cc54d73e8f3ae44c1d70fbd3fc3f33
Bocdagla <tomeu1m1a1@gmail.com>
2024-05-15 22:37:50 +0200

    Add colors

--- scripts/gameplay/building_creation/buy_building_button.gd
+++ scripts/gameplay/building_creation/buy_building_button.gd
@@ -1,15 +1,11 @@
 class_name BuyBuildingButton;
 extends Button;
 
+const BUY_BUILDING_BUTTON_CONTENT = preload("res://prefabs/gameplay/building_creation/buy_building_button_content.tscn")
+
 @onready var _resource_manager: ResourceManager = %ResourceManager;
 
 @export var buildin_record: BuildingRecord;
-
-@export_category("UI")
-@onready var _gold_label: Label = $MarginContainer/VBoxContainer/Label/GoldLabel;
-@onready var _defense_icon: TextureRect = $MarginContainer/VBoxContainer/Label/DefenseIcon;
-@onready var _defense_label: Label = $MarginContainer/VBoxContainer/Label/DefenseLabel;
-
 
 var _pressed_callables: Array[Callable];
 
@@ -23,11 +19,9 @@ func _ready() -> void:
 
 	_resource_manager.value_changed.connect(_set_available);
 	text = buildin_record.name;
-	_gold_label.text = str(buildin_record.gold_cost);
-	_defense_label.text = str(buildin_record.defense_cost);
-	if buildin_record.defense_cost == 0:
-		_defense_label.visible = false;
-		_defense_icon.visible = false;
+	var content = BUY_BUILDING_BUTTON_CONTENT.instantiate() as BuyBuildingButtonContent;
+	add_child(content);
+	content.setup(buildin_record);
 
 
 func _pressed() -> void:
