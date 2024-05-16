44aa2026a44c0316a7d11130f3638ffe53c1ed97
Bocdagla <tomeu1m1a1@gmail.com>
2024-05-16 17:38:14 +0200

    turn based

--- scripts/gameplay/game_flow/event_manager.gd
+++ scripts/gameplay/game_flow/event_manager.gd
@@ -1,11 +1,36 @@
-extends Node
+class_name EventManager;
+extends Node;
 
+@onready var resource_manager: ResourceManager = %ResourceManager
+@onready var event_ui: CanvasLayer = %EventUI
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var first_attack_turn := 10;
+@export var first_attack_defense := 10;
+@export var second_attack_turn := 20;
+@export var second_attack_defense := 10;
+@export var last_attack_turn := 30;
+@export var last_attack_defense := 10;
 
+signal crucial_event_started;
+signal final_event_started();
+signal crucial_event_resolved(success: bool);
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var _current_event_defense_cost: int = 0;
+
+func _on_turn_start(turn: int):
+	match turn:
+		first_attack_turn:
+			_current_event_defense_cost = first_attack_defense;
+			crucial_event_started.emit();
+		second_attack_turn:
+			_current_event_defense_cost = second_attack_defense;
+			crucial_event_started.emit();
+		last_attack_turn:
+			_current_event_defense_cost = last_attack_defense;
+			final_event_started.emit();
+
+func _on_crucial_event_continue():
+	if resource_manager.is_affordable(0, _current_event_defense_cost):
+		crucial_event_resolved.emit(true);
+	else:
+		crucial_event_resolved.emit(false);
--- scripts/gameplay/game_flow/game_manager.gd
+++ scripts/gameplay/game_flow/game_manager.gd
@@ -1,83 +1,102 @@
 class_name GameManager;
 extends Node
 
-@export var first_attack_turn := 10;
-@export var first_attack_defense := 10;
-@export var second_attack_turn := 20;
-@export var second_attack_defense := 10;
-@export var last_attack_turn := 30;
-@export var last_attack_defense := 10;
+@onready var event_manager: EventManager = $"../EventManager"
 
-@onready var build_ui: CanvasLayer = %BuildUI
 @onready var main_camera: Camera3D = %MainCamera
-@onready var turn_manager: TurnManager = %TurnManager
-@onready var resource_manager: ResourceManager = %ResourceManager
 @onready var game_over_ui: CanvasLayer = %GameOverUI
+@onready var victory_ui: CanvasLayer = %VictoryUI
+@onready var event_ui: CanvasLayer = %EventUI
+@onready var build_ui: CanvasLayer = %BuildUI
 
-enum GAME_STATE { STARTING, TUTORIAL, PLAYING, FIRST_ATTACK, SECOND_ATTACK, FINAL_ATTACK, GAMEOVER };
+enum GAME_STATE { STARTING, TUTORIAL, PLAYING, CRUCIAL_EVENT_HAPPENING, FINAL_EVENT_HAPPENING, GAMEOVER, VICTORY };
 var state: GAME_STATE = GAME_STATE.STARTING;
 
 func _ready() -> void:
-	turn_manager.turn_started.connect(_on_turn_start);
 	call_deferred("_on_tutorial_finished");
+	event_manager.crucial_event_started.connect(_on_crucial_event_started);
+	event_manager.final_event_started.connect(_on_final_event_started);
+	event_manager.crucial_event_resolved.connect(_on_crucial_event_resolved);
 
 func _change_state(new_state: GAME_STATE):
 	match new_state:
 		GAME_STATE.STARTING:
 			game_over_ui.visible = false;
+			victory_ui.visible = false;
 			build_ui.visible = false;
+			event_ui.visible = false;
 			pass
 
 		GAME_STATE.TUTORIAL:
 			print("tutorial");
 			game_over_ui.visible = false;
+			victory_ui.visible = false;
 			build_ui.visible = false;
+			event_ui.visible = false;
 			pass
 
 		GAME_STATE.PLAYING:
 			game_over_ui.visible = false;
+			victory_ui.visible = false;
 			build_ui.visible = true;
+			event_ui.visible = false;
 			pass
 
-		GAME_STATE.FIRST_ATTACK:
-			print("Firsst Attack");
+		GAME_STATE.CRUCIAL_EVENT_HAPPENING:
+			print("CRUCIAL EVENT");
 			game_over_ui.visible = false;
+			victory_ui.visible = false;
 			build_ui.visible = false;
-			_change_state(GAME_STATE.GAMEOVER);
+			event_ui.visible = true;
 			pass
 
-		GAME_STATE.SECOND_ATTACK:
-			print("Second Attack");
+		GAME_STATE.FINAL_EVENT_HAPPENING:
+			print("FINAL EVENT");
 			game_over_ui.visible = false;
+			victory_ui.visible = false;
 			build_ui.visible = false;
-			_change_state(GAME_STATE.GAMEOVER);
-			pass
-
-		GAME_STATE.FINAL_ATTACK:
-			print("Final Attack");
-			game_over_ui.visible = false;
-			build_ui.visible = false;
-			_change_state(GAME_STATE.GAMEOVER);
+			event_ui.visible = true;
 			pass
 
 		GAME_STATE.GAMEOVER:
 			build_ui.visible = false;
 			game_over_ui.visible = true;
+			victory_ui.visible = false;
+			event_ui.visible = false;
 			print("Game Over");
 			pass
+
+		GAME_STATE.VICTORY:
+			build_ui.visible = false;
+			game_over_ui.visible = false;
+			victory_ui.visible = true;
+			event_ui.visible = false;
+			print("Victory");
+			pass
+	state = new_state;
 
 func _on_tutorial_finished():
 	_change_state(GAME_STATE.PLAYING);
 
-func _on_turn_start(turn: int):
-	match turn:
-		first_attack_turn:
-			_change_state(GAME_STATE.FIRST_ATTACK);
-		second_attack_turn:
-			_change_state(GAME_STATE.SECOND_ATTACK);
-		last_attack_turn:
-			_change_state(GAME_STATE.FINAL_ATTACK)
-
 func _on_restart_pressed():
 	get_tree().reload_current_scene();
 
+func _on_crucial_event_started() -> void:
+	_change_state(GAME_STATE.CRUCIAL_EVENT_HAPPENING);
+
+func _on_final_event_started() -> void:
+	_change_state(GAME_STATE.FINAL_EVENT_HAPPENING);
+
+
+func _on_crucial_event_resolved(success: bool) -> void:
+	match state:
+		GAME_STATE.CRUCIAL_EVENT_HAPPENING:
+			if success:
+				_change_state(GAME_STATE.PLAYING);
+			else:
+				_change_state(GAME_STATE.GAMEOVER);
+		GAME_STATE.FINAL_EVENT_HAPPENING:
+			if success:
+				_change_state(GAME_STATE.VICTORY);
+			else:
+				_change_state(GAME_STATE.GAMEOVER);
